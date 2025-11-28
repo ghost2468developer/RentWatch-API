46524da5484109b48e2bf0181f41c3350d992bd4
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2025-11-28 16:28:43 +0200

    feat(auth): set up JWT token

--- config/env.ts
+++ config/env.ts
@@ -4,5 +4,6 @@ dotenv.config()
 export const env = {
   PORT: process.env.PORT || 4000,
   DATABASE_URL: process.env.DATABASE_URL!,
-  JWT_SECRET: process.env.JWT_SECRET || "supersecretkey"
+  JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "9000d"
 }
