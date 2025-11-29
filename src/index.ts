6bcc63e4a617fd04866717ee6da14b865897702d
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2025-11-29 09:49:59 +0200

    feat(server): server set up

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,30 @@ import morgan from "morgan"
 
 // Routes
 import userRoutes from "./modules/user/routes"
+// import propertyRoutes from "./modules/property/routes"
+// import leaseRoutes from "./modules/lease/routes"
+// import paymentRoutes from "./modules/payment/routes"
+// import disputeRoutes from "./modules/dispute/routes"
 
 // Middlewares
 import { errorHandler } from "./middlewares/errorHandler"
+import { env } from "./config/env"
 
-const app = express();
+const app = express()
 
 // Middlewares
-app.use(cors());
+app.use(cors())
 app.use(express.json())
-app.use(morgan("dev")) // logs requests
+app.use(morgan("dev"))
 
-// API Routes
+// API Router
 app.use("/api/users", userRoutes)
 // app.use("/api/properties", propertyRoutes)
 // app.use("/api/leases", leaseRoutes)
 // app.use("/api/payments", paymentRoutes)
 // app.use("/api/disputes", disputeRoutes)
 
-// Health Check
+// Health check
 app.get("/", (req, res) => {
   res.send("🚀 RentWatch API is running")
 })
@@ -30,4 +35,7 @@ app.get("/", (req, res) => {
 // Error handling middleware
 app.use(errorHandler)
 
-export default app
+// Start the server
+app.listen(env.PORT, () => {
+  console.log(`🚀 RentWatch API running on port ${env.PORT}`)
+})
